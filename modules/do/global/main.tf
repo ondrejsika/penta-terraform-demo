039eae93ba3f935b39331a53f5298a8d1b0f4a0c
Ondrej Sika <ondrej@ondrejsika.com>
2021-04-15 07:44:23 +0200

    feat(do): Add ubuntu image & medium size

--- modules/do/global/main.tf
+++ modules/do/global/main.tf
@@ -10,9 +10,11 @@ output "conf" {
     region = var.region
     images = {
       debian = "debian-10-x64"
+      ubuntu = "ubuntu-18-10-x64"
     }
     sizes = {
-      small = "s-1vcpu-1gb"
+      small  = "s-1vcpu-1gb"
+      medium = "s-2vcpu-2gb"
     }
   }
 }
